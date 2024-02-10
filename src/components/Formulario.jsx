8517cb39acbdb8f9ff43d639af48e2e59aca1255
Maximiliano Marigno <marignomaxy@gmail.com>
2024-02-10 20:24:10 -0300

    modificaciones boton

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,6 +10,7 @@ function Formulario() {
   const [apellido, setApellido] = useState('')
   const [edad, setEdad] = useState('')
   const [tomaAlcohol, setTomaAlcohol] = useState(false)
+  const [confirmation, setConfirmation] = useState(false)
 
   const handleSwitchChange = () => {
     setTomaAlcohol(!tomaAlcohol)
@@ -25,7 +26,7 @@ function Formulario() {
     }
     try {
       const response = await guardarInvitado(data)
-      setAlert({ variant: 'success', text: `Usted a sido confirmado,lo esperamos` })
+      setConfirmation(true)
       console.log(response)
     } catch (error) {
       setAlert({ variant: 'danger', text: `Usted no ha sido confirmado` })
@@ -36,32 +37,36 @@ function Formulario() {
   return (
     <>
       <AlertCustom {...alert} />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-1 text-center" controlId="formBasicNombre">
-          <Form.Label className="Montserrat-bold fs-2">Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-        </Form.Group>
-
-        <Form.Group className="mb-1 text-center" controlId="formBasicApellido">
-          <Form.Label className="Montserrat-bold fs-2">Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} />
-        </Form.Group>
-
-        <Form.Group className="mb-3 text-center" controlId="formBasicEdad">
-          <Form.Label className="Montserrat-bold fs-2">Edad</Form.Label>
-          <Form.Control type="number" placeholder="Ingrese su edad" value={edad} onChange={(e) => setEdad(e.target.value)} />
-        </Form.Group>
-
-        {parseInt(edad, 10) >= 18 && (
-          <Form.Group className="mb-1 text-center fs-2" controlId="formBasicSwitch">
-            <Form.Check type="switch" id="custom-switch" label="¿Toma alcohol?" className="colorSwitch" checked={tomaAlcohol} onChange={handleSwitchChange} />
+      {confirmation ? (
+        <h2 className="text-center">Gracias por confirmar.</h2>
+      ) : (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-1 text-center" controlId="formBasicNombre">
+            <Form.Label className="Montserrat-bold fs-2">Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Ingrese su nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
           </Form.Group>
-        )}
 
-        <Button className="btn-lg w-100 cambioFS200 colorBoton Abril mb-5" type="submit">
-          Confirmar
-        </Button>
-      </Form>
+          <Form.Group className="mb-1 text-center" controlId="formBasicApellido">
+            <Form.Label className="Montserrat-bold fs-2">Apellido</Form.Label>
+            <Form.Control type="text" placeholder="Ingrese su apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+          </Form.Group>
+
+          <Form.Group className="mb-3 text-center" controlId="formBasicEdad">
+            <Form.Label className="Montserrat-bold fs-2">Edad</Form.Label>
+            <Form.Control type="number" placeholder="Ingrese su edad" value={edad} onChange={(e) => setEdad(e.target.value)} />
+          </Form.Group>
+
+          {parseInt(edad, 10) >= 18 && (
+            <Form.Group className="mb-1 text-center fs-2" controlId="formBasicSwitch">
+              <Form.Check type="switch" id="custom-switch" label="¿Toma alcohol?" className="colorSwitch" checked={tomaAlcohol} onChange={handleSwitchChange} />
+            </Form.Group>
+          )}
+
+          <Button className="btn-lg w-100 cambioFS200 colorBoton Abril mb-5" type="submit">
+            Confirmar
+          </Button>
+        </Form>
+      )}
     </>
   )
 }
