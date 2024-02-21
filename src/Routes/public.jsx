967c00ed082dd5b609eebc864444ce164ae7113b
Maximiliano Marigno <marignomaxy@gmail.com>
2024-02-21 00:14:55 -0300

    cambios

--- src/Routes/public.jsx
+++ src/Routes/public.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
+import Invitados from '../pages/Invitados'
 
 function Public() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/invitados" element={<Invitados />} />
     </Routes>
   )
 }
--- src/services/invitadosServices.js
+++ src/services/invitadosServices.js
@@ -3,7 +3,3 @@ import axios from 'axios'
 export const guardarInvitado = (data) => {
   return axios.post(`https://apiinvitacion-production.up.railway.app/`, data)
 }
-
-export const getInvitados = () => {
-  return axios.get(`https://apiinvitacion-production.up.railway.app/`)
-}
